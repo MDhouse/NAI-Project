30abcc08bf10971bd04ee90e3467b0a7c375fdb1
Damian Junko <damian.junko@gmail.com>
2017-01-26 20:07:14 +0100

    Create library for detect and convert color image

---
+++ Detection/ConvertToHsv.cs
@@ -0,0 +1,33 @@
+﻿namespace Detection
+{
+    using System;
+
+    using Emgu.CV;
+    using Emgu.CV.Structure;
+
+    public class ConvertToHsv : IConvertColor
+    {
+        /// <summary>
+        /// The detect image.
+        /// </summary>
+        /// <param name="image">
+        /// The image.
+        /// </param>
+        /// <param name="minColor">
+        /// The min color.
+        /// </param>
+        /// <param name="maxColor">
+        /// The max color.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Image"/>.
+        /// </returns>
+        internal override Image<Gray, byte> DetectImage(Image<Bgr, byte> image, IColor minColor, IColor maxColor)
+        {
+            var imageConvertToHsv = image.Convert<Hsv, byte>();
+            var imageDetection = imageConvertToHsv.InRange((Hsv)minColor, (Hsv)maxColor);
+
+            return imageDetection;
+        }
+    }
+}
---
+++ Detection/IConvertColor.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IConvertColor.cs" company="">
+// </copyright>
+// <summary>
+//   Defines the IConvertColor type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Detection
+{
+    using Emgu.CV;
+    using Emgu.CV.Structure;
+
+    /// <summary>
+    /// The convert color.
+    /// </summary>
+    public abstract class IConvertColor
+    {
+        internal abstract Image<Gray, byte> DetectImage(Image<Bgr, byte> image, IColor minColor, IColor maxColor);
+    }
+}
