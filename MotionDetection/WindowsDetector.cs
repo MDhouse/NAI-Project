39bc8e5a682689bfaf1b7a64200007be79820683
Damian Junko <damian.junko@gmail.com>
2017-01-27 22:22:37 +0100

    Slight refactoring

--- MotionDetection/WindowsDetector.cs
+++ MotionDetection/WindowsDetector.cs
@@ -17,53 +17,98 @@ namespace MotionDetection
     using Emgu.CV.UI;
     using Emgu.CV.Cuda;
 
+    /// <summary>
+    /// The windows detector.
+    /// </summary>
     public partial class WindowsDetector : Form
     {
-        Image<Bgr, byte> currentImage;
+        /// <summary>
+        /// Gets the capture.
+        /// </summary>
+        public new Capture Capture { get; }
 
-        Image<Bgr, byte> currentImageCopy;
+        /// <summary>
+        /// Gets the current image.
+        /// </summary>
+        public Image<Bgr, byte> CurrentImage { get; private set; }
 
-        Capture capture;
-        //AdaptiveSkinDetector 
+        /// <summary>
+        /// Gets the current image copy.
+        /// </summary>
+        public Image<Bgr, byte> CurrentImageCopy { get; private set; }
 
+        /// <summary>
+        /// Gets or sets the min hsv.
+        /// </summary>
+        public Hsv MinHsv { get; set; }
+
+        /// <summary>
+        /// Gets or sets the max hsv.
+        /// </summary>
+        public Hsv MaxHsv { get; set; }
+
+        /// <summary>
+        /// Gets or sets the mi hsv.
+        /// </summary>
+        public Hsv MiHsv { get; set; }
+
+        /// <summary>
+        /// Gets or sets the max ycc.
+        /// </summary>
+        public Ycc MaxYcc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the min ycc.
+        /// </summary>
+        public Ycc MinYcc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the frame width.
+        /// </summary>
+        public int FrameWidth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the frame height.
+        /// </summary>
+        public int FrameHeight { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WindowsDetector"/> class.
+        /// </summary>
         public WindowsDetector()
         {
             this.InitializeComponent();
-            this.capture = new Emgu.CV.Capture();
-            this.capture.QueryFrame();
+            this.Capture = new Emgu.CV.Capture();
+            this.Capture.QueryFrame();
 
-            var frameWidth = this.capture.Width;
-            var frameHeight = this.capture.Height;
+            this.FrameHeight = this.Capture.Height;
+            this.FrameWidth = this.Capture.Width;
 
-            var hsvMin = new Hsv(0, 45, 0);
-            var hsvMax = new Hsv(20, 255, 255);
-            var yccMin = new Ycc(0, 131, 80);
-            var yccMax = new Ycc(255, 185, 135);
+            this.MinHsv = new Hsv(0, 45, 0);
+            this.MaxHsv = new Hsv(20, 255, 255);
+            this.MinYcc = new Ycc(0, 131, 80);
+            this.MaxYcc = new Ycc(255, 185, 135);
 
             Application.Idle += new EventHandler(this.FrameCapture);
         }
 
+        /// <summary>
+        /// The frame capture.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
         private void FrameCapture(object sender, EventArgs e)
         {
-            this.currentImage = this.capture.QueryFrame().ToImage<Bgr, byte>();
+            this.CurrentImage = this.Capture.QueryFrame().ToImage<Bgr, byte>();
 
-            if (this.currentImage != null)
+            if (this.CurrentImage != null)
             {
-                this._imageBox.Image = this.currentImage;
-
-
+                this._imageBox.Image = this.CurrentImage;
             }
-
-        }
-
-        private void _imageBox_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void _imageToBox_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
